508a85fe2ef1875866e8fefa409954763a589a8d
ShuEugene <shueugene@hotmail.com>
2022-09-08 19:37:26 +0300

    HW5.2.1 complete.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 // Домашнее задание к уроку №5 «Циклы» (части второй)
 public class Main {
     public static void main(String[] args) {
+//  ДЗ 5.2.1
+        {
+            int salary = 29_000, cashSavings = 0, monthNumber = 1;
+            while (cashSavings <= 2_459_000) {
+                cashSavings += (cashSavings / 100) + salary;
+                System.out.printf("Месяц %d, сумма накоплений равна %d рублей.\n",
+                        monthNumber, cashSavings);
+                monthNumber++;
+            }
+        }
 
+        System.out.println();
     }
 }
